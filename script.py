1bed9fac6274866c7e62bd47dca118ab4f2a49cd
David Ooi <45958279+OoiHS@users.noreply.github.com>
2024-11-14 11:27:28 +0800

    Update script.py

--- script.py
+++ script.py
@@ -1,6 +1,7 @@
-import os
+import subprocess
 
-try:
-  os.system("mkdir testdir")
-except Exception as e:
-  print("fail")
+command = "mkdir testdir"
+process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+output, error = process.communicate()
+print( output )
+print( error )
