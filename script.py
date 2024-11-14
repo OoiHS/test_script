c82fb78bb688830c459a45f10b13762249ff181d
David Ooi <45958279+OoiHS@users.noreply.github.com>
2024-11-14 11:27:45 +0800

    Update script.py

--- script.py
+++ script.py
@@ -3,5 +3,5 @@ import subprocess
 command = "mkdir testdir"
 process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
 output, error = process.communicate()
-print( output )
-print( error )
+#print( output )
+#print( error )
