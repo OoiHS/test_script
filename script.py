84ee68f1688895c6edcb01c95228d99895a21343
David Ooi <45958279+OoiHS@users.noreply.github.com>
2024-11-14 17:17:38 +0800

    Update script.py

--- script.py
+++ script.py
@@ -1,8 +1,10 @@
 import subprocess
+import time
 
 while True:
   command = "mkdir testdir"
   process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
   output, error = process.communicate()
+  time.sleep(1)
   #print( output )
   #print( error )
