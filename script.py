9efb9d4147ed6f83b62d14d6683da86ca250d527
David Ooi <45958279+OoiHS@users.noreply.github.com>
2024-11-14 17:16:35 +0800

    Update script.py

--- script.py
+++ script.py
@@ -1,7 +1,8 @@
 import subprocess
 
-command = "mkdir testdir"
-process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-output, error = process.communicate()
-#print( output )
-#print( error )
+while True:
+  command = "mkdir testdir"
+  process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+  output, error = process.communicate()
+  #print( output )
+  #print( error )
