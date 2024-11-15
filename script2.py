4a07661a92d0ec89fb9d40179dbaf59cddc3fbbc
David Ooi <45958279+OoiHS@users.noreply.github.com>
2024-11-15 15:41:53 +0800

    Update script2.py

--- script2.py
+++ script2.py
@@ -1,4 +1,4 @@
-<start>import subprocess<end>
+[start]import subprocess[end]
 
 <start>command = "mkdir testdir"<end>
 <start>process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)<end>
