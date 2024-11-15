4ad096e1487b92ba3535e615d6f0df471149e059
David Ooi <45958279+OoiHS@users.noreply.github.com>
2024-11-15 15:42:57 +0800

    Update script2.py

--- script2.py
+++ script2.py
@@ -1,8 +1,8 @@
 [start]import subprocess[end]
 
-<start>command = "mkdir testdir"<end>
-<start>process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)<end>
-<start>output, error = process.communicate()<end>
-<start>#print( output )<end>
-<start>#print( error )<end>
+[start]command = "mkdir testdir"[end]
+[start]process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)[end]
+[start]output, error = process.communicate()[end]
+[start]#print( output )[end]
+[start]#print( error )[end]
 
