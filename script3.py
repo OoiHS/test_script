affe00b060fd1a5ee9f4bdefb960a1f09fa2f54c
David Ooi <45958279+OoiHS@users.noreply.github.com>
2024-11-16 01:40:15 +0800

    Update script3.py

--- script3.py
+++ script3.py
@@ -1,1 +1,1 @@
-[start]LZIlU64gVYLZ?JWZ@F5bW;EjVmbi?Z=[Wl\aTKEbWJ=XVYIlLY`hW:IhW6\]UYWiGY`cF9@iW:ImW9`mLZ=cV;DiV`I`Vl`bMJ9^Vl`gLJ_h?ZA]V`_jW65_<6`\TJ[iL`9mT64R?YE_W\`hWJkf<7<R=]8J[end]
+[start]L&IlU64gV%LZ?J#Z@F5bW;EjVmbi?&=[Wl(aTKEbWJ=$V%IlL%,hW:IhW6(]U%#iG%,cF9@iW:ImW9,mL&=cV;DiV`I`Vl,bMJ9^Vl,gLJ_h?&A]V`_jW65+<6,\TJ'iL`9mT64|?%E_W\,hWJkf<7<|=]80[end]
